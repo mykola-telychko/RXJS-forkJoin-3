d3e7f97bcf64dae6b4f4fd7e944e7f455d57448e
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-08 04:59:21 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,13 +1,18 @@
 import { mergeMap } from 'rxjs/operators';
 import { forkJoin, of } from 'rxjs';
 
-const myPromise = (val) =>
+// https://www.learnrxjs.io/learn-rxjs/operators/combination/forkjoin
+
+const samplePromise = (val) =>
   new Promise((resolve) =>
     setTimeout(() => resolve(`Promise Resolved: ${val}`), 5000)
   );
 
-const source = of([1, 2, 3, 4, 5]);
-//emit array of all 5 results
-const example = source.pipe(mergeMap((q) => forkJoin(...q.map(myPromise))));
+const srcArr = of([1, 2, 3, 4, 5]);
+// emit array of all 5 results
+// marge srcArr and samplePromise results using forkJoin
+const example = srcArr.pipe(
+  mergeMap((item) => forkJoin(...item.map(samplePromise)))
+);
 
 const subscribe = example.subscribe((val) => console.log(val));
